a8c7cc1412d295c4eaa7b1795d59248fd3bde65b
Anton <antosha.uminsky@gmail.com>
2022-11-21 21:00:16 +0300

    change directories again

--- University/src/StudentException.java
+++
@@ -1,9 +0,0 @@
-public class StudentException extends  Exception {
-    public StudentException(String message) {
-        super(message);
-    }
-
-    public StudentException(String message, Throwable e){
-        super(message, e);
-    }
-}
